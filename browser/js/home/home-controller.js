51f9bf09ad435d9da067be2594205016d80596bb
nmadd <nate@fratmusic.com>
2016-04-06 10:48:49 -0400

    finally got menu transition working

--- browser/js/home/home-controller.js
+++ browser/js/home/home-controller.js
@@ -1,6 +1,8 @@
 app.controller('HomeController', function($scope){
 	$scope.showMenu = false;
 
+	$scope.styleTest = '4em'
+
     $scope.toggle = function(){
         console.log('toggleys')
         $scope.showMenu = !$scope.showMenu;
